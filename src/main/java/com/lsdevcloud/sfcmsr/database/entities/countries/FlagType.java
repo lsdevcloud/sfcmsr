245cf1460f9fd136b9d80baf1e114d52dacc4900
lsdevcloud <info@lsdevcloud.com>
2025-03-16 13:19:12 +0100

    [Flags]: add FlagType

---
+++ src/main/java/com/lsdevcloud/sfcmsr/database/entities/countries/FlagType.java
@@ -0,0 +1,20 @@
+package com.lsdevcloud.sfcmsr.database.entities.countries;
+
+/**
+ * Created: 30.11.24
+ *
+ * @author lsdevcloud (leo@lsdevcloud.com)
+ */
+public enum FlagType
+{
+    INTERACT("interact"),
+    PLACE("place"),
+    BREAK("break"),
+    HARM("harm"),
+    ;
+
+    FlagType(String s)
+    {
+
+    }
+}
